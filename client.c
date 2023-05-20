28a26729435b8d24b751ce8031cfe09561733470
Wei-Wu2002 <131269595+Wei-Wu2002@users.noreply.github.com>
2023-05-20 14:08:10 +0800

    Add files via upload

---
+++ client.c
@@ -0,0 +1,10 @@
+// 2023.5.20 v1，目标：实现wireshark可成功解析的DNS报文
+
+int main(int argc, char *agrv) {
+    // 定义UDP结构体（参考lab4）
+
+
+    // 定义DNS报文首部
+
+    // 向DNS服务器发送报文请求
+}
---
+++ server.c
@@ -0,0 +1,7 @@
+// 2023.5.20 v1，目标：实现wireshark可成功解析的DNS报文
+
+int main(void) {
+    // 定义UDP结构体
+    // 定义DNS报文首部
+    // 接受DNS报文
+}
